a472bb4e083a3c138c3756b4f6c375496bdbf664
Arjun Mishra <arjunmishra13@gmail.com>
2017-02-12 16:33:45 -0600

    Stack problem 1

--- src/crackingthecodinginterview/problems/UseSingleArrayToImplementThreeStacks.java
+++ src/crackingthecodinginterview/problems/UseSingleArrayToImplementThreeStacks.java
@@ -7,11 +7,11 @@ package crackingthecodinginterview.problems;
  */
 public class UseSingleArrayToImplementThreeStacks {
 
-	private int stack1Index, stack2Index, stack3Index;
-	private int eachStackSize;
+	private int stack1Index = -1, stack2Index = -1, stack3Index = -1;//Represents the last element index for that stack
+//	private int eachStackSize;
 	private int[]arr;
 	public UseSingleArrayToImplementThreeStacks(int size) {
-		this.eachStackSize = size;
+//		this.eachStackSize = size;
 		this.arr = new int[size*3];
 	}
 	
@@ -19,35 +19,37 @@ public class UseSingleArrayToImplementThreeStacks {
 		
 		switch(stack) {
 		case 1:
-			if(stack1Index >= eachStackSize) {
+			if(stack1Index >= arr.length) {
 				throw new IndexOutOfBoundsException();
 			}
-			arr[stack1Index] = element;
-			if(stack2Index == stack1Index + 1) {
+			if( stack1Index != -1 && stack3Index != stack1Index) {
 				shiftUp(stack1Index);
 			}
 			stack1Index++;
 			stack2Index++;
 			stack3Index++;
+			arr[stack1Index] = element;
 			break;
 		case 2:
-			if(stack2Index >= eachStackSize) {
+			if(stack2Index >= arr.length) {
 				throw new IndexOutOfBoundsException();
 			}
-			arr[stack2Index] = element;
-			if(stack3Index == stack2Index + 1) {
+			if(stack2Index != -1 && stack3Index != stack2Index) {
 				shiftUp(stack2Index);
 			}
 			stack2Index++;
 			stack3Index++;
+			arr[stack2Index] = element;
 			break;
 		case 3:
-			if(stack3Index >= eachStackSize) {
+			if(stack3Index >= arr.length) {
 				throw new IndexOutOfBoundsException();
 			}
-			arr[stack1Index] = element;
-			shiftUp(stack3Index);
+			if(stack3Index != -1) {
+				shiftUp(stack3Index);
+			}
 			stack3Index++;
+			arr[stack3Index] = element;
 			break;
 		default:
 			throw new Exception("Invalid stack id. Choose values between 1 - 3");
@@ -62,34 +64,45 @@ public class UseSingleArrayToImplementThreeStacks {
 			last--;
 		}
 	}
+
+	private void shiftDown(int start) {
+		
+		while(start < stack3Index) {
+			arr[start] = arr[start + 1];
+			start++;
+		}
+		arr[stack3Index] = 0;
+	}
 	
 	public int pop(int stack) throws IndexOutOfBoundsException, Exception {
 		int outElement = 0;
 		switch(stack) {
 		case 1:
-			if(stack1Index == 0) {
+			if(stack1Index == -1) {
 				throw new IndexOutOfBoundsException();
 			}
-			stack1Index--;
 			outElement = arr[stack1Index];
-			arr[stack1Index] = 0;
-			shiftUp(stack1Index);
+			shiftDown(stack1Index);
+			stack1Index--;
+			stack2Index--;
+			stack3Index--;
 			break;
 		case 2:
-			if(stack2Index <= stack1Index + 1) {
+			if(stack2Index == -1 || (stack2Index == stack1Index)) {
 				throw new IndexOutOfBoundsException();
 			}
-			stack2Index--;
 			outElement = arr[stack2Index];
-			arr[stack2Index] = 0;
+			shiftDown(stack2Index);
+			stack2Index--;
+			stack3Index--;
 			break;
 		case 3:
-			if(stack3Index <= stack2Index + 1) {
+			if(stack3Index == -1 || (stack3Index == stack2Index)) {
 				throw new IndexOutOfBoundsException();
 			}
+			outElement = arr[stack3Index];
+			shiftDown(stack3Index);
 			stack3Index--;
-			outElement = arr[stack1Index];
-			arr[stack3Index] = 0;
 			break;
 		default:
 			throw new Exception("Invalid stack id. Choose values between 1 - 3");
@@ -102,20 +115,20 @@ public class UseSingleArrayToImplementThreeStacks {
 	public int top(int stack) throws IndexOutOfBoundsException, Exception {
 		switch(stack) {
 		case 1:
-			if(stack1Index == 0) {
+			if(stack1Index == -1) {
 				throw new IndexOutOfBoundsException();
 			}
-			return arr[stack1Index - 1];
+			return arr[stack1Index];
 		case 2:
-			if(stack2Index <= stack1Index + 1) {
+			if(stack2Index == -1 || (stack2Index == stack1Index)) {
 				throw new IndexOutOfBoundsException();
 			}
-			return arr[stack2Index-1];
+			return arr[stack2Index];
 		case 3:
-			if(stack3Index <= stack2Index + 1) {
+			if(stack3Index == -1 || (stack3Index == stack2Index)) {
 				throw new IndexOutOfBoundsException();
 			}
-			return arr[stack3Index - 1];
+			return arr[stack3Index];
 		default:
 			throw new Exception("Invalid stack id. Choose values between 1 - 3");
 		}
@@ -125,8 +138,24 @@ public class UseSingleArrayToImplementThreeStacks {
 		int size = 5;
 		
 		UseSingleArrayToImplementThreeStacks stack = new UseSingleArrayToImplementThreeStacks(size);
-		stack.push(5, 1);
+		stack.push(1, 1);
+		stack.push(2, 1);
+		System.out.println(stack.top(1));
+		stack.push(3, 1);
+		stack.push(1, 2);
+		System.out.println(stack.top(2));
+		System.out.println(stack.pop(1));
+		System.out.println(stack.pop(1));
+		System.out.println(stack.pop(2));
 		stack.push(2, 2);
-		stack.push(7, 3);
+		stack.push(3, 2);
+		stack.push(1, 3);
+		stack.push(2, 3);
+		System.out.println(stack.pop(3));
+		System.out.println(stack.pop(3));
+		stack.push(3, 3);
+		stack.push(4, 3);
+		System.out.println(stack.pop(3));
+		System.out.println(stack.top(3));
 	}
 }
