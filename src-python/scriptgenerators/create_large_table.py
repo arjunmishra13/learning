8b012c3057c1e2ea2022659bc2c8a786ca6ce465
amishra <amishra@cloudera.com>
2018-09-28 16:20:32 -0500

    Create large table script

---
+++ src-python/scriptgenerators/create_large_table.py
@@ -0,0 +1,59 @@
+import sys
+
+TBL = "tbl"
+COL = "col"
+SEMICOLON = ";"
+EQUALS = "="
+SPACE = " "
+DOT = "."
+COMMA = ","
+FORWARD_SLASH = "/"
+SINGLE_QUOTES = "'"
+CURLY_OPEN_BRACKETS = "("
+CURLY_CLOSE_BRACKETS = ")"
+DOT_DB = ".db"
+END_CHARACTER = "\n"
+INT_TYPE = "INT"
+def create_large_table(db_name, table_name, number_of_columns, number_of_partitions, outputfile):
+
+  if number_of_columns < 2:
+    raise ValueError('number_of_columns needs to be atleast 2')
+
+  outputF = open(outputfile, 'w')
+
+  create_table_command1 = "CREATE TABLE " + db_name + DOT + table_name + SPACE + CURLY_OPEN_BRACKETS
+  create_table_command2 = "PARTITIONED BY "
+  location_str = "LOCATION '/user/hive/warehouse/"
+
+  table_command = create_table_command1
+  for col_val in range(1, number_of_columns):
+
+    if col_val < number_of_columns - 1:
+      table_command = table_command + COL + str(col_val) + SPACE + INT_TYPE + COMMA + SPACE
+    else:
+      table_command = table_command + COL + str(col_val) + SPACE + INT_TYPE + CURLY_CLOSE_BRACKETS + SPACE
+
+
+  table_command = table_command + create_table_command2 + CURLY_OPEN_BRACKETS + COL + str(number_of_columns) + SPACE + INT_TYPE + CURLY_CLOSE_BRACKETS + SPACE
+  table_command = table_command + location_str + db_name + DOT_DB + FORWARD_SLASH + SINGLE_QUOTES + SEMICOLON + END_CHARACTER
+
+  outputF.write(table_command)
+
+  create_partition_command1 = "ALTER TABLE "
+  create_partition_command2 = "ADD PARTITION "
+
+  base_partition_command = create_partition_command1 + db_name + DOT + table_name + SPACE + create_partition_command2 + CURLY_OPEN_BRACKETS
+  for part_val in range(1, number_of_partitions + 1):
+    partition_command = base_partition_command + COL + str(number_of_columns) + SPACE + EQUALS + SPACE + str(part_val) + CURLY_CLOSE_BRACKETS
+    partition_command = partition_command + SPACE + location_str + db_name + DOT_DB + FORWARD_SLASH + table_name + FORWARD_SLASH + SINGLE_QUOTES + SEMICOLON + END_CHARACTER
+    outputF.write(partition_command)
+
+  outputF.close();
+
+if __name__ == "__main__":
+  db_name = "dbx"
+  table_name = "tblx"
+  number_of_columns = 100
+  number_of_partitions = 1000
+  outputfile = "/Users/amishra/Documents/Cloudera/TestFiles/GenerateDataScripts/large_table.txt"
+  create_large_table(db_name, table_name, number_of_columns, number_of_partitions, outputfile)
